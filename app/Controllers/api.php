b54bca80a0c801f43c2dacf1d4919237e010e8b0
ihsandulu <ihsan.dulu@.com>
2024-04-02 07:40:46 +0700

    api upload gambar

--- app/Controllers/api.php
+++ app/Controllers/api.php
@@ -929,6 +929,14 @@ class api extends baseController
         return $this->response->setContentType('application/json')->setJSON($data);
     }
 
+    public function absen1(){
+        header('Access-Control-Allow-Origin: *');
+        //print_r(json_encode($_FILES));
+        $new_image_name = urldecode($_FILES["file"]["name"]).".jpg";
+        //Move your files into upload folder
+        move_uploaded_file($_FILES["file"]["tmp_name"], "images/absen_picture/".$new_image_name);
+    }
+
     public function absen(){
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Headers: Content-Type');
