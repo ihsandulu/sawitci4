24361a94ae97456c2bc0c8869885efcbdce9da53
ihsandulu <ihsan.dulu@.com>
2024-04-08 15:01:33 +0700

    quarry number

--- app/Views/template/headerside_v.php
+++ app/Views/template/headerside_v.php
@@ -242,6 +242,24 @@
                 <li> 
                     <a class="  " href="<?= base_url("msptbsnumber"); ?>" aria-expanded="false"><i class="fa fa-id-card"></i><span class="hide-menu">SPTBS Card</span></a>
                 </li>
+                <?php }?>    
+                <?php 
+                if (
+                    (
+                        isset(session()->get("position_id")[0][0]) 
+                        && (
+                            session()->get("position_id") == "1" 
+                            || session()->get("position_id") == "2"
+                        )
+                    ) ||
+                    (
+                        isset(session()->get("halaman")['64']['act_read']) 
+                        && session()->get("halaman")['64']['act_read'] == "1"
+                    )
+                ) { ?>
+                <li> 
+                    <a class="  " href="<?= base_url("mquarrynumber"); ?>" aria-expanded="false"><i class="fa fa-id-card"></i><span class="hide-menu">QUARRY Card</span></a>
+                </li>
                 <?php }?>   
                 <?php 
                 if (
