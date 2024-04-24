3ba50c73230dda4fcce18885a5e15c4c3e29cca8
ihsandulu <ihsan.dulu@.com>
2024-04-24 10:47:14 +0700

    nilai array di quarry pengirim kosong

--- app/Views/transaction/quarry_v.php
+++ app/Views/transaction/quarry_v.php
@@ -656,7 +656,7 @@
                                             </td>
                                             <td class="text-left"><?= $usr->drivername; ?></td>
                                             <td>
-                                                <?= $usr->wt_name; ?> (<?= $usr->wt_jenis; ?> <?= $vendor[$usr->wt_vendor]; ?> <?= $sewa[$usr->wt_sewa]; ?>)
+                                                <?= $usr->wt_name; ?> (<?= $usr->wt_jenis; ?> <?= ($usr->wt_vendor!="")?$vendor[$usr->wt_vendor]:""; ?> <?= ($usr->wt_sewa!="")?$sewa[$usr->wt_sewa]:""; ?>)
                                             </td>
                                             <td><?= $usr->pengirimname; ?></td>
                                             <td><?= $usr->quarry_jampergi; ?></td>
